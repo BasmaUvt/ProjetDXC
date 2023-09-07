498bee5cdab0585530326e8af89eb1fa1009b677
brakrouki2 <besma.rakrouki2@dxc.com>
2023-09-07 22:00:28 +0100

    jenkins file update

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000/',
-    specPattern: '**/*index_spec.js',
+    specPattern: '**/*chat-spec.js',
     // other e2e options here
   },
 })
