4de596712711440ad913f8798f44c44150aaee63
brakrouki2 <besma.rakrouki2@dxc.com>
2023-09-10 01:58:14 +0100

    new change

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000/',
     specPattern: '**/*chat-spec.js',
+    // specPattern: '**/*index-spec.js',
     // other e2e options here
   },
 })
