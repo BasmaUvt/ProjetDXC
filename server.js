ad465f98285d7af8992511692483a0b41c44eb7a
brakrouki2 <besma.rakrouki2@dxc.com>
2023-10-16 20:49:46 +0100

    new update

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 const io = socketIO(server);
 const PORT = process.env.PORT || 3000;
 
-const uri = 'mongodb://172.23.32.1:27017/ma_base_de_donnees';
+const uri = 'mongodb://localhost:27017/ma_base_de_donnees';
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true});
 
 const db = mongoose.connection;
