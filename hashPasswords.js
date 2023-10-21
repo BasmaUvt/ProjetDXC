5c7ac9119d05236be50a30005aa5c4a006a24c54
brakrouki2 <besma.rakrouki2@dxc.com>
2023-10-21 22:00:44 +0100

    update files

--- hashPasswords.js
+++ hashPasswords.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const bcrypt = require('bcryptjs');
 
-const uri = "mongodb://127.0.0.1:27017/ma_base_de_donnees";
+const uri = "mongodb://192.168.224.1:27017/ma_base_de_donnees";
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
